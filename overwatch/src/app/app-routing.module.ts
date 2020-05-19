425c56cba8369c4f8176069c5da93c1f54a4d48b
Athul Joy <aathuljjoy333@gmail.com>
2020-05-19 16:44:49 +0530

    updated router order

--- overwatch/src/app/app-routing.module.ts
+++ overwatch/src/app/app-routing.module.ts
@@ -5,22 +5,32 @@ import { RegisterComponent } from './components/register/register.component'
 import { AuthGuard } from './guards/auth.guard'
 import { UploadimagesComponent } from './components/uploadimages/uploadimages.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
-import {  ProfileComponent } from './components/profile/profile.component'
+import { ProfileComponent } from './components/profile/profile.component'
 import { HomeComponent } from './components/home/home.component';
 import {SearchculpritComponent } from './components/searchculprit/searchculprit.component'
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component'
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path :'home', component:HomeComponent},
-  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'createlookoutdata/:id', 
+  { path: 'login', component: LoginComponent },
+  { path: 'createlookoutdata', 
     component:UploadimagesComponent,
     canActivate:[AuthGuard]
   },
-  { path: 'search', component:SearchculpritComponent },
-  { path: 'gallery',component:GalleryComponent },
-  { path :'profile',component:ProfileComponent },
+  { path: 'search', 
+    component:SearchculpritComponent,
+    canActivate:[AuthGuard] },
+  { path: 'gallery',
+    component:GalleryComponent,
+    canActivate:[AuthGuard]
+  },
+  { path :'profile',
+    component:ProfileComponent,
+    canActivate:[AuthGuard]
+  },
+  { path : '**', component:PagenotfoundComponent}
   
 ];
 
