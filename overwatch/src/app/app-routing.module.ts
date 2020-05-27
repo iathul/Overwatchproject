c8c076d0e20333507695dbddfa98313ac7d78f12
Athul Joy <aathuljjoy333@gmail.com>
2020-05-27 11:40:25 +0530

    added auth-guard

--- overwatch/src/app/app-routing.module.ts
+++ overwatch/src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   },
   { path: 'gallery',
     component:GalleryComponent,
-    //canActivate:[AuthGuard]
+    canActivate:[AuthGuard]
   },
   { path :'profile',
     component:ProfileComponent,
